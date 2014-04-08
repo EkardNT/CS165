622d56526464716b67150f526ff89f4523dfe77d
Drake Tetreault <ekardnt@ekardnt.com>
2014-04-08 12:06:50 -0700

    Initial correct version finished.

--- CS 165/main.cpp
+++ CS 165/main.cpp
@@ -1,72 +1,80 @@
-#include "compare.c"
+#include <cstring>
+#include <cstdio>
+#include <ctime>
 
-const int MAX_N = 1000000;
-const int MAX_K = 1000000;
-const int K_OUT_OF_RANGE = -1;
-const int WRONG_INDICES = -1000;
-const int RUNS_PER_CASE = 10;
+#define MEMOIZED_TYPE char
 
-int indices[MAX_N];
-int best[MAX_K + 1];
+/* Forward declarations for compare functions. */
+int COMPARE(int arg1, int arg2, ...);
+double dshrandom(long input);
 
-bool greaterThan(int index1, int index2);
+/* Constants */
+const int MaxN = 10000;
+const int MaxK = 40;
+const int RunsPerCase = 100;
 
-void selectionSort(int count);
-void quickSort(int count);
+/* Data structures */
+// Will hold the indices of the k greatest elements in descending order.
+int best[MaxK + 1]; 
+// A min heap of indices.
+int heap[MaxK]; 
+// A record of all comparisons, stored as a 2D array,
+// where the value of the array element is:
+//	0 if the elements have not been compared.
+//	1 if the first index is greater than the second index.
+//	2 if the first index is lesser than the second index.
+MEMOIZED_TYPE memoizedComparisons[MaxN * MaxN]; 
 
-void doalg(int n, int k)
-{
-	if (n > MAX_N || k > MAX_K)
-	{
-		printf("N or K is too large, max K is %d, max N is %d.", MAX_N, MAX_K);
-		return;
-	}
+/* Status variables */
+// Total number of elements that are currently being searched.
+int n = 0;
+// Number of elements that must be found.
+int k = 0;
+// Number of elements in the heap, equivalent to 
+// the first unoccupied heap array index.
+int heapSize = 0;
+MEMOIZED_TYPE memoizedBase = 0;
 
-	// Initialize elements and indices.
-	COMPARE(0, n);
-	for (int i = 0; i < n; i++)
-		indices[i] = i + 1; // Elements indexed starting from 1, not 0.
-	
-	// Sort the indices in descending order, then copy
-	// the highest k indices into best.
-	selectionSort(n);
-	for (int i = 0; i < k; i++)
-		best[i + 1] = indices[i];
-}
+/* Operations */
+void init_alg(int n, int k);
+void doalg(int n, int k);
+bool greater_than(int index1, int index2);
+void heap_push(int element);
+int heap_pop();
+int heap_parent(int childSlot);
+int heap_left_child(int parentSlot);
+int heap_right_child(int parentSlot);
+int heap_lesser_child(int parentSlot);
+void heap_swap(int& slot1, int& slot2);
+void printArray(int * arr, int count);
 
 struct Case
 {
 	int n, k;
-
 	Case(int n, int k) : n(n), k(k) {}
 };
 
-void printArray(int * arr, int count)
-{
-	printf("[");
-	for (int i = 0; i < count - 1; i++)
-		printf("%d, ", arr[i]);
-	printf("%d]", arr[count - 1]);
-}
-
 int main()
 {
-	Case cases[4] = { Case(15, 3), Case(100, 10), Case(1000, 20), Case(10000, 40) };
+	Case cases[] = { Case(15, 3), Case(100, 10), Case(1000, 20), Case(10000, 40) };
 
 	for (int c = 0; c < 4; c++)
 	{
 		int maxComparisons = 0,
 			totalComparisons = 0;
+		memoizedBase = 0;
 
-		for (int i = 0; i < RUNS_PER_CASE; i++)
+		for (int i = 0; i < RunsPerCase; i++)
 		{
+			dshrandom(std::time(nullptr));
 			doalg(cases[c].n, cases[c].k);
+			memoizedBase += 2;
 
 			int result = COMPARE(-1, cases[c].k, best);
 
-			if (result == K_OUT_OF_RANGE)
+			if (result == -1)
 				printf("k (%d) is out of range.\n", cases[c].k);
-			else if (result == WRONG_INDICES)
+			else if (result == -1000)
 				printf("doalg() failed to return the correct indices for the %d greatest elements.\n", cases[c].k);
 			else
 			{
@@ -76,7 +84,7 @@ int main()
 			}
 		}
 
-		printf("For n=%d and k=%d, max comparisons is %d and average comparisons is %f.\n\n", cases[c].n, cases[c].k, maxComparisons, totalComparisons / (double)RUNS_PER_CASE);
+		printf("For n=%d and k=%d, max comparisons is %d and average comparisons is %f.\n\n", cases[c].n, cases[c].k, maxComparisons, totalComparisons / (double)RunsPerCase);
 	}
 
 	getchar();
@@ -84,56 +92,170 @@ int main()
 	return 0;
 }
 
-bool greaterThan(int index1, int index2)
+void printArray(int * arr, int count)
 {
-	return COMPARE(index1, index2) == 1;
+	printf("[");
+	for (int i = 0; i < count - 1; i++)
+		printf("%d, ", arr[i]);
+	printf("%d]", arr[count - 1]);
 }
 
-void selectionSort(int* indices, int count)
+void init_alg(int n, int k)
 {
-	// i is the first unsorted element.
-	for (int i = 0; i < count; i++)
-	{
-		int max = i;
-		for (int j = i + 1; j < count; j++)
-		{
-			if (greaterThan(indices[j], indices[max]))
-				max = j;
-		}
-		int temp = indices[max];
-		indices[max] = indices[i];
-		indices[i] = temp;
-	}
-}
+	::n = n;
+	::k = k;
 
-int getQuicksortPivot(int first, int last)
-{
-	// Median of first, middle, last.
-	int middle = first + (last - first) / 2;
-	if (greaterThan(indices[first], indices[last]))
-	{
-		if (greaterThan(indices[first], indices[middle]))
-			return greaterThan(indices[last], indices[middle]) ? last : middle;
-		else
-			return first;
-	}
-	else
-	{
-		if (greaterThan(indices[last], indices[middle]))
-			return greaterThan(indices[first], indices[middle]) ? first : middle;
-		else
-			return last;
-	}
-}
-
-void quickSortRange(int first, int last)
-{
-	if (first >= last)
-		return;
+	// Clear best array and heap.
+	std::memset(best, 0, k + 1);
+	std::memset(heap, 0, k);
+	heapSize = 0;
+	// Clear memoized comparisons.
+	for (int i = 0; i < n * n; i++)
+		memoizedComparisons[i] = 0;
 	
+	COMPARE(0, n);
 }
 
-void quickSort(int count)
+void doalg(int n, int k)
 {
-	quickSortRange(0, count - 1);
+	init_alg(n, k);
+
+	// Add all indices to heap. Remember indices start at 1.
+	for (int i = 1; i <= n; i++)
+		heap_push(i);
+
+	// Remove k elements from heap and put them in descending
+	// order in the best array.
+	for (int i = 0; i < k; i++)
+		best[k - i] = heap_pop();
+}
+
+bool greater_than(int index1, int index2)
+{
+	index1--;
+	index2--;
+	MEMOIZED_TYPE memoized = memoizedComparisons[index1 + index2 * n];
+	// Already computed?
+	if (memoized >(MEMOIZED_TYPE)0)
+		return memoized == (MEMOIZED_TYPE)1;
+	// Otherwise, compute and check return value for error.
+	int result = COMPARE(index1 + 1, index2 + 1);
+	if (result == -1)
+	{
+		printf("greater_than() called with out of bounds indices %d and/or %d.", index1, index2);
+		return false;
+	}
+	// Record the result in both possible locations.
+	memoizedComparisons[index1 + index2 * n] = (MEMOIZED_TYPE)result;
+	memoizedComparisons[index2 + index1 * n] = (MEMOIZED_TYPE)(result == 1 ? 2 : 1);
+	return result == 1;
+}
+
+void heap_walk_down(int startSlot)
+{
+	int currentSlot = startSlot;
+	int lesserChildSlot = heap_lesser_child(currentSlot);
+	// Move the new element down the heap while it has
+	// children and the new element is greater than
+	// the lesser of its two children.
+	while (currentSlot != lesserChildSlot 
+		&& greater_than(heap[currentSlot], heap[lesserChildSlot]))
+	{
+		heap_swap(currentSlot, lesserChildSlot);
+		lesserChildSlot = heap_lesser_child(currentSlot);
+	}
+}
+
+void heap_push(int element)
+{
+	// If the heap is not full, then we need to add
+	// the element to the first available spot then
+	// walk it up to its proper place.
+	if (heapSize < k)
+	{
+		heap[heapSize] = element;
+		int currentSlot = heapSize;
+		int parentSlot = heap_parent(currentSlot);
+		heapSize++;
+		// Move the new element up the heap while it hasn't
+		// reached the root and it is less than its parent.
+		while (currentSlot > 0 
+			&& !greater_than(heap[currentSlot], heap[parentSlot]))
+		{
+			heap_swap(currentSlot, parentSlot);
+			parentSlot = heap_parent(currentSlot);
+		}
+		return;
+	}
+	// Otherwise, the heap is full. Although this
+	// is a min-heap, we are only interested in the
+	// k greatest elements, so we don't need to add
+	// the element to the heap if it is less than
+	// the root (minimum) element.
+	if (greater_than(heap[0], element))
+		return;
+	// Finally, we know the heap is full and the
+	// new element is greater than the minimum
+	// element currently contained in the heap. To
+	// add the new element, we replace the existing
+	// root (minimum) element with the new element,
+	// then walk it down the heap to where it belongs.
+	heap[0] = element;
+	heap_walk_down(0);
+	// Note we replaced an element, so the heap size
+	// does not change.
+}
+
+int heap_pop()
+{
+	// Check for empty error case.
+	if (heapSize == 0)
+	{
+		printf("Called heap_pop() but the heap is empty.");
+		return -1;
+	}
+	int minimalElement = heap[0];
+	// Replace the root with the last leaf, then walk
+	// the last leaf down until it arrives at its proper
+	// place.
+	heap[0] = heap[heapSize - 1];
+	heapSize--;
+	heap_walk_down(0);
+	return minimalElement;
+}
+
+int heap_parent(int childSlot)
+{
+	return (childSlot - 1) / 2;
+}
+
+int heap_left_child(int parentSlot)
+{
+	return parentSlot * 2 + 1;
+}
+
+int heap_right_child(int parentSlot)
+{
+	return parentSlot * 2 + 2;
+}
+
+void heap_swap(int& slot1, int& slot2)
+{
+	int elementTemp = heap[slot1];
+	heap[slot1] = heap[slot2];
+	heap[slot2] = elementTemp;
+	int slotTemp = slot1;
+	slot1 = slot2;
+	slot2 = slotTemp;
+}
+
+int heap_lesser_child(int parentSlot)
+{
+	int leftSlot = heap_left_child(parentSlot),
+		rightSlot = heap_right_child(parentSlot);
+	if (leftSlot >= heapSize)
+		return parentSlot;
+	if (rightSlot >= heapSize)
+		return leftSlot;
+	return greater_than(heap[leftSlot], heap[rightSlot]) ? rightSlot : leftSlot;
 }
